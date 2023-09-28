0c9907cd559201f37b4cd0ae7c8999f99cdfa5c5
PierrotAWB <andrewwang298@gmail.com>
2023-09-28 15:18:13 -0400

    Revert "Add colors and context"
    This reverts commit 2d0a0d525927f7b2a117883134c043cf2bafc325.

--- App.js
+++ App.js
@@ -2,23 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 
 import Example from "./app/components/Example";
-import { ThemeProvider } from "./app/contexts/ThemeContext"
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <View style={styles.container}>
-        <Example />
-        <StatusBar style="auto" />
-      </View>
-    </ThemeProvider >
+    <View style={styles.container}>
+      <Example />
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "dodgerblue",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
--- app/components/Example.js
+++ app/components/Example.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { useTheme } from '../contexts/ThemeContext';
 
-function Example() {
-  const { theme } = useTheme();
-  const styles = getStyles(theme)
-
+function Example(props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.message}>This is an example component. All other components should go in ./app/components</Text>
+      <Text>This is an example component. All other components should go in ./app/components</Text>
     </View>
   );
 }
 
-// Recall that hooks can't be called outside
-// components. So, in order for the following
-// styles object to know about 'theme', it
-// must be passed as an 
-const getStyles = (theme) => StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    backgroundColor: theme.background,
     margin: 10,
-  },
-  message: {
-    color: theme.foreground,
   }
 });
 export default Example;
