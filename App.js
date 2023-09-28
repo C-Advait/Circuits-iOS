2d0a0d525927f7b2a117883134c043cf2bafc325
PierrotAWB <andrewwang298@gmail.com>
2023-09-28 15:17:07 -0400

    Add colors and context

--- App.js
+++ App.js
@@ -2,20 +2,23 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 
 import Example from "./app/components/Example";
+import { ThemeProvider } from "./app/contexts/ThemeContext"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Example />
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <View style={styles.container}>
+        <Example />
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "dodgerblue",
     alignItems: "center",
     justifyContent: "center",
   },
