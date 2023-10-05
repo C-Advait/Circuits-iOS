778b7cb67bcbbc01044c9dba3289ffa6059eb361
Advait Singh Chauhan <advait.c123@gmail.com>
2023-10-05 16:16:28 -0400

    update how accentcolor works, to provide consistent spacing when accentcolor is false

--- app/components/AuxiliaryCard.js
+++ app/components/AuxiliaryCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, ActionSheetIOS } from "react-native";
 
 import { useTheme } from "../contexts/ThemeContext";
 import EditableText from "./EditableText";
@@ -15,6 +15,7 @@ function AuxiliaryCard({
 }) {
   const { theme } = useTheme();
   const styles = getStyles(theme);
+  accentcolor = accentcolor || theme.backgroundFaded;
 
   const fontWeight = bold ? 600 : 400;
   const textStyle = [styles.header, { fontWeight: fontWeight }];
@@ -26,9 +27,7 @@ function AuxiliaryCard({
           <Icon />
         </View>
       ) : null}
-      {accentcolor ? (
-        <View style={[styles.accent, { backgroundColor: accentcolor }]} />
-      ) : null}
+      <View style={[styles.accent, { backgroundColor: accentcolor }]} />
       <View style={styles.contentContainer}>
         {editable ? (
           <EditableText placeholder={title} style={textStyle} />
