7c3d96af0e150bf61d5e2470d67c0c0aaf563fbc
PierrotAWB <andrewwang298@gmail.com>
2023-10-24 13:44:22 -0400

    Store settings with AsyncStorage

--- app/contexts/SettingsContext.js
+++ app/contexts/SettingsContext.js
@@ -1,5 +1,7 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import { lightTheme, darkTheme } from "../config/colors";
+import { retrieveSetting, saveSetting } from "../db/asyncStorage";
+import { SETTINGS_KEYS } from "../config/settingsKeys";
 
 export const SettingsContext = createContext();
 
@@ -12,13 +14,31 @@ export const SettingsProvider = ({ children }) => {
     setTheme(theme === lightTheme ? darkTheme : lightTheme);
   };
 
+  // Load settings from asyncStorage initially.
+  useEffect(() => {
+    const loadSettings = async () => {
+      const setting = JSON.parse(await retrieveSetting(SETTINGS_KEYS.SOUND));
+      if (setting) {
+        setSoundOn(setting);
+      }
+    };
+
+    loadSettings();
+  }, []);
+
+  // Right now handles only sound settings.
+  const updateSound = async (value) => {
+    await saveSetting(SETTINGS_KEYS.SOUND, JSON.stringify(value));
+    setSoundOn(value);
+  };
+
   return (
     <SettingsContext.Provider
       value={{
         theme,
         toggleTheme,
         soundOn,
-        setSoundOn,
+        updateSound,
       }}
     >
       {children}
