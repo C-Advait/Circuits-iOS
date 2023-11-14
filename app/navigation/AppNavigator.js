87e581b0b913d12590c2ab894f8c4ec04a5a4414
PierrotAWB <andrewwang298@gmail.com>
2023-11-13 19:20:59 -0500

    Remove debug screen

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -4,7 +4,6 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import routes from "./routes";
 import RoutineNavigator from "./RoutineNavigator";
 import AppTabBar from "./AppTabBar";
-import DebugScreen from "../screens/DebugScreen";
 import SettingsNavigator from "./SettingsNavigator";
 
 const Tab = createBottomTabNavigator();
@@ -19,7 +18,6 @@ function AppNavigator() {
     >
       <Tab.Screen name={routes.ROUTINES} component={RoutineNavigator} />
       <Tab.Screen name={routes.SETTINGS} component={SettingsNavigator} />
-      <Tab.Screen name={routes.DEBUG_SCREEN} component={DebugScreen} />
     </Tab.Navigator>
   );
 }
