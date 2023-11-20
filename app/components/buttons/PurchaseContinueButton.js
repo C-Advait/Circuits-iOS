90f471fefd828f4c7c2631bf2901d8df867fbb05
PierrotAWB <andrewwang298@gmail.com>
2023-11-20 14:24:24 -0500

    Switch overlay loader to lottie

--- app/components/buttons/PurchaseContinueButton.js
+++ app/components/buttons/PurchaseContinueButton.js
@@ -6,20 +6,32 @@ import {
   PARAGRAPH_FONT_SIZE,
   PARAGRAPH_FONT_WEIGHT,
 } from "../../config/appConstants";
+import LottieView from "lottie-react-native";
 
-function PurchaseContinueButton({ onPress }) {
+function PurchaseContinueButton({ loading, onPress }) {
   const { theme } = useAppContext();
   const styles = getStyles(theme);
 
   return (
     <View style={{ width: "100%" }}>
-      <TouchableOpacity
-        style={styles.background}
-        onPress={onPress}
-        activeOpacity={0.8}
-      >
-        <Text style={styles.text}>Continue</Text>
-      </TouchableOpacity>
+      {loading ? (
+        <View style={styles.background}>
+          <LottieView
+            source={require("../../assets/lotties/loading.json")}
+            autoPlay
+            loop={true}
+            style={{ height: 100, width: 100 }}
+          />
+        </View>
+      ) : (
+        <TouchableOpacity
+          style={styles.background}
+          onPress={onPress}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.text}>Continue</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
@@ -27,8 +39,11 @@ function PurchaseContinueButton({ onPress }) {
 const getStyles = (theme) =>
   StyleSheet.create({
     background: {
+      alignItems: "center",
       backgroundColor: theme.accentDarkBlue,
       borderRadius: 10,
+      flexDirection: "row",
+      overflow: "hidden",
       height: 58,
       justifyContent: "center",
       marginBottom: 20,
